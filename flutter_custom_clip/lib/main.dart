7954a9a850fab5367f377a6db320a91bfe536653
sidiq08 <m.aulia08@mail.unpas.ac.id>
2022-07-01 07:03:28 +0700

    font clippath yt pak erico

---
+++ flutter_custom_clip/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Latihan Clipper"),
+      ),
+      body: Center(
+        child: ClipPath(
+          clipper: MyClipper(),
+          child: Image(
+              width: 300,
+              image: NetworkImage(
+                  "https://images.unsplash.com/photo-1653933889748-560501f80a6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80")),
+        ),
+      ),
+    ));
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(
+        size.width / 2, size.height * 0.5, size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) => false;
+}
